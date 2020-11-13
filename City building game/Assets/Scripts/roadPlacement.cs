b35322dfd80d7e8491493f88e76d56de854c3925
zbynek666 <zbynek66666@gmail.com>
2020-11-13 15:05:46 +0100

    lot of things

--- City building game/Assets/Scripts/roadPlacement.cs
+++ City building game/Assets/Scripts/roadPlacement.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,6 +11,8 @@ public class roadPlacement : MonoBehaviour , IPointerClickHandler
 
     private GameObject building2;
     private RaycastHit hit;
+    private Vector2 p1;
+    private Vector2 p2;
 
 
     public void OnPointerClick(PointerEventData eventData)
@@ -24,15 +28,34 @@ public class roadPlacement : MonoBehaviour , IPointerClickHandler
     // Update is called once per frame  
     void Update()
     {
-        if (building != null)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
-            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        if (p1 == null) 
+                        {
+                            p1 = hit.point;
+                        }
+                        else if(p2 == null) 
+                        {
+                            p2 = hit.point;
+                    //createRoad();
+                        }
+                    }
+                }
 
-                building.transform.position = new Vector3(Mathf.Round(hit.point.x), 1, Mathf.Round(hit.point.z));
-            }
-        }
 
     }
+
+    private void createRoad(float x1,float x2) 
+    {
+        
+    }
+
+    private float getPointInGame(float x) 
+    {
+        return Mathf.Round(x);
+    }
+
 }
