e141f090096da08ee7000ec34303fcc3cd3183d3
zbynek666 <zbynek66666@gmail.com>
2020-11-14 18:00:52 +0100

    recreate placement system

---
+++ City building game/Assets/Scripts/blueprint_script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blueprint_script : MonoBehaviour
+{
+
+    RaycastHit hit;
+    Vector3 movepoint;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = hit.point;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit,5000.0f,1))
+        {
+            transform.position = hit.point;
+            Debug.Log(hit.point);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+
+            Instantiate(prefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ City building game/Assets/Scripts/buldingButtonsManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buldingButtonsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject prefab;
+
+    public void spawnBlueprint() 
+    {
+        Instantiate(prefab);
+    }
+        
+}
