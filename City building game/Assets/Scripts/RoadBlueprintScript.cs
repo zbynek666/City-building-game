e03d915fdb9f7c7e20835365c2c4e7ad342c0568
zbynek666 <zbynek66666@gmail.com>
2020-11-28 13:19:18 +0100

    dono

---
+++ City building game/Assets/Scripts/RoadBlueprintScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBlueprintScript : MonoBehaviour
+{
+    private int roadStart = -5000;
+    private int roadEnd = -5000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0) && roadStart != -5000)
+        {
+            setSrartPoint();
+        }
+        else if(Input.GetMouseButton(0) && roadEnd != -5000)
+        {
+            buildRoad();
+        }
+
+    }
+
+    private void buildRoad()
+    {
+
+    }
+
+    private void setSrartPoint()
+    {
+
+    }
+}
